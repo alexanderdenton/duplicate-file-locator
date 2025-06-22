d24987b678d3826424f5b4c205c5807295de3699
Alexander Denton <AlexanderTDenton@outlook.com>
2025-06-22 19:22:00 +0100

    Added Test project.

---
+++ DuplicateFileLocatorTests/DuplicateFileLocatorTests.cs
@@ -0,0 +1,12 @@
+﻿using DuplicateFileLocatorLibrary.Classes;
+using DuplicateFileLocatorLibrary.Interfaces;
+using NUnit.Framework;
+
+namespace DuplicateFileLocatorTests
+{
+    [TestFixture]
+    public class DuplicateFileLocatorTests
+    {
+        IDuplicateFileLocator duplicateFileLocator = new DuplicateFileLocator();
+    }
+}
