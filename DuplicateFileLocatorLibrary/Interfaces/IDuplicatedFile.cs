d291980c95dfa633b2fa481bfcaa2935e0dcdeaa
Alexander Denton <AlexanderTDenton@outlook.com>
2025-06-22 19:45:55 +0100

    Added DuplicatedFile class.

---
+++ DuplicateFileLocatorLibrary/Interfaces/IDuplicatedFile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateFileLocatorLibrary.Interfaces
+{
+    public interface IDuplicatedFile
+    {
+        #region Attributes
+
+        /// <summary>
+        /// Hash value of file.
+        /// </summary>
+        string Hash { get; }
+        
+        /// <summary>
+        /// Path to first file found.
+        /// </summary>
+        string OriginalPath { get; set; }
+
+        /// <summary>
+        /// List of paths to duplicate files.
+        /// </summary>
+        List<string> DuplicatePaths { get; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Method adds path to <c>DuplicatePaths</c>.
+        /// </summary>
+        /// <param name="path"></param>
+        void AddDuplicatePath(string path);
+
+        #endregion
+    }
+}
