efdafc12317ec153d3d1e6cee319b7a6d14d0854
Alexander Denton <AlexanderTDenton@outlook.com>
2025-06-28 20:47:51 +0100

    Console has been tested based on inputs. It works.

--- DuplicateFileLocatorConsole/Program.cs
+++ DuplicateFileLocatorConsole/Program.cs
@@ -7,16 +7,19 @@ namespace DuplicateFileLocatorConsole
     {
         static void Main(string[] args)
         {
-            IDuplicateFileLocator duplicateFileLocator = new DuplicateFileLocator();
+            //IDuplicateFileLocator duplicateFileLocator = new DuplicateFileLocator();
 
             if (args.Length != 0)
             {
+                Console.WriteLine(args[0]);
                 if (args[0] == "-f" || args[0] == "--find")
                 {
+                    Console.WriteLine("Option Find");
                     if (args.Length > 1)
                     {
-                        string pathToFolder = args[1];
-                        duplicateFileLocator.FindDuplicateFiles(pathToFolder);
+                        Console.WriteLine(args[1]);
+                        //string pathToFolder = args[1];
+                        //duplicateFileLocator.FindDuplicateFiles(pathToFolder);
                     }
                     else
                     {
@@ -25,34 +28,42 @@ namespace DuplicateFileLocatorConsole
                 }
                 else if (args[0] == "-d" || args[0] == "--display-duplicate-log")
                 {
-                    duplicateFileLocator.DisplayDuplicateFiles();
+                    Console.WriteLine("Option Display");
+                    //duplicateFileLocator.DisplayDuplicateFiles();
                 }
                 else if (args[0] == "-c" || args[0] == "--clear-duplicate-log")
                 {
-                    duplicateFileLocator.ClearDuplicateFiles();
+                    Console.WriteLine("Option Clear");
+                    //duplicateFileLocator.ClearDuplicateFiles();
                 }
                 else if (args[0] == "-e" || args[0] == "--export-duplicate-log")
                 {
+                    Console.WriteLine("Option Export");
                     if (args.Length > 1)
                     {
-                        string exportPath = args[1];
-                        duplicateFileLocator.ExportDuplicateFiles(exportPath);
+                        Console.WriteLine(args[1]);
+                        //string exportPath = args[1];
+                        //duplicateFileLocator.ExportDuplicateFiles(exportPath);
                     }
                     else
                     {
-                        duplicateFileLocator.ExportDuplicateFiles();
+                        Console.WriteLine("No Path");
+                        //duplicateFileLocator.ExportDuplicateFiles();
                     }
                 }
                 else if (args[0] == "-v" || args[0] == "--verify-duplicate-log")
                 {
-                    duplicateFileLocator.VerifyDuplicateFiles();
+                    Console.WriteLine("Option Verify");
+                    //duplicateFileLocator.VerifyDuplicateFiles();
                 }
                 else if (args[0] == "-h" || args[0] == "--hash-file")
                 {
+                    Console.WriteLine("Option Hash");
                     if (args.Length > 1)
                     {
-                        string pathToFile = args[1];
-                        duplicateFileLocator.HashIndividualFile(pathToFile);
+                        Console.WriteLine(args[1]);
+                        //string pathToFile = args[1];
+                        //duplicateFileLocator.HashIndividualFile(pathToFile);
                     }
                     else
                     {
