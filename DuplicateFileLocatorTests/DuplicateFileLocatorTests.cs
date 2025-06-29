669d18d0b6f0347647f6e17b58f8afc3d40cdbad
Alexander Denton <AlexanderTDenton@outlook.com>
2025-06-29 12:29:41 +0100

    Completed Tests

--- DuplicateFileLocatorTests/DuplicateFileLocatorTests.cs
+++ DuplicateFileLocatorTests/DuplicateFileLocatorTests.cs
@@ -7,6 +7,46 @@ namespace DuplicateFileLocatorTests
     [TestFixture]
     public class DuplicateFileLocatorTests
     {
-        IDuplicateFileLocator duplicateFileLocator = new DuplicateFileLocator();
+        [Test]
+        public void DuplicateFileLocator_Constuctor()
+        {
+            string testJson = "C:\\Users\\Alexa\\repos\\duplicate-file-locator\\test.json";
+            using (StreamWriter sw = File.CreateText(testJson))
+            {
+                sw.WriteLine();
+            }
+
+            IDuplicateFileLocator duplicateFileLocator = new DuplicateFileLocator(testJson);
+            
+            Assert.That(duplicateFileLocator, Is.Not.Null);
+        }
+
+        [Test]
+        public void FindDuplicateFiles()
+        {
+            string testJson = "C:\\Users\\Alexa\\repos\\duplicate-file-locator\\test.json";
+            using (StreamWriter sw = File.CreateText(testJson))
+            {
+                sw.WriteLine();
+            }
+
+            IDuplicateFileLocator duplicateFileLocator = new DuplicateFileLocator(testJson);
+
+            duplicateFileLocator.FindDuplicateFiles("C:\\Users\\Alexa\\repos\\duplicate-file-locator\\test_files");
+
+            string jsonResults = string.Empty;
+            using (StreamReader sr = new StreamReader("C:\\Users\\Alexa\\repos\\duplicate-file-locator\\test.json"))
+            {
+                jsonResults = sr.ReadToEnd();
+            }
+
+            string jsonExpected = string.Empty;
+            using (StreamReader sr = new StreamReader("C:\\Users\\Alexa\\repos\\duplicate-file-locator\\test-expected.json"))
+            {
+                jsonExpected = sr.ReadToEnd();
+            }
+
+            Assert.That(jsonResults, Is.EqualTo(jsonExpected));
+        }
     }
 }
